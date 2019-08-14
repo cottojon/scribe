017a74421896ee6c59489d8dc01228b4fe20a2e3
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 14:28:41 -0400

    user entity and repository created

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,8 @@
+import { Repository, EntityRepository } from "typeorm";
+import { User } from "./user.entity";
+
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+
+}
