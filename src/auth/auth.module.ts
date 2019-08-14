abbe2836a7b4346bfa8a87f3e0529b07c0b64c10
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 14:30:49 -0400

    auth.module updated to import userRepository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
-@Module({
+@Module({ //import User Repo for dependency injection in the auth module
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
