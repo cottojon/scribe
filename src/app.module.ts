68284633b14de2639fcb3dfa6ecacd28a807640c
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 14:25:58 -0400

    app.module.ts updated to import typeorm module with given options

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChannelsModule } from './channels/channels.module';
 import { ClipsModule } from './clips/clips.module';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule} from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [ChannelsModule, ClipsModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ChannelsModule, ClipsModule, AuthModule],
   controllers: [],
   providers: [],
 })
