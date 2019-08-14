d95df2f939a67080fe70a2b53ff648c5ec7e8716
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 14:32:22 -0400

    auth service dependency inject User repository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    // inject our user repo when the service is initialized
+    constructor( @InjectRepository(UserRepository) private userRepository: UserRepository){}
+}
