c970e5a6e0f9291e94e8057fa498ba955e476cce
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-14 14:20:23 -0400

    auth module, controller, service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChannelsModule } from './channels/channels.module';
 import { ClipsModule } from './clips/clips.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [ChannelsModule, ClipsModule],
+  imports: [ChannelsModule, ClipsModule, AuthModule],
   controllers: [],
   providers: [],
 })
