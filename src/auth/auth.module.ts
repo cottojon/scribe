2ee007fa1f65925995911c2ac966cd77c917ad7c
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-19 12:17:32 -0400

    import jwt and passport modules in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-
-@Module({ //import User Repo for dependency injection in the auth module
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+import {JwtModule} from '@nestjs/jwt';
+import {PassportModule} from '@nestjs/passport';
+@Module({ 
+  imports: [ // provide the strategy passport it gonna use
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.register({
+    secret: 'topSecret51', // the secret we will use in our token, we will chane this later
+    signOptions: {
+      expiresIn: 3600, // 3600 seconds == 1 hr
+    },
+  }),
+    TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
