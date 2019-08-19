8b882a2865ea7ffb7decc33707df075a4fed4e1c
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-19 12:56:22 -0400

    create test POST handler to see if our jwt strategy/custom decorator works

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { GetUser } from './get-user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +20,13 @@ export class AuthController {
     @Post('/signin') // validation pipe added for authCredentialsDto
     signin(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}>{
         return this.authService.signIn(authCredentialsDto);
+    }
+
+
+    // to test our jwt strategy and getuser decorator
+    @Post('/test')
+    @UseGuards(AuthGuard()) // guard/require authorization using jwt strategy class
+    test(@GetUser() user){
+        console.log(user);
     }
 }
