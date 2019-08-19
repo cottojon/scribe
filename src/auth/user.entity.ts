087cc455e862b513950ec5832f126cd0145f8e9b
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-19 11:23:44 -0400

    validatePassword method added to user entity class

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 @Unique(['username']) // expects an array of column names
@@ -15,4 +16,15 @@ export class User extends BaseEntity{
 
     @Column()
     salt: string;
+
+
+
+    // custom mehtod to validate our password
+    // receive in a password from the client and hash it with user salt
+    // compare user password(hash) with generated hash
+    // if equal return true else false
+    async validatePassword(password: string): Promise<boolean>{
+        const hash = await bcrypt.hash(password, this.salt);
+        return hash === this.password;
+    }
 }
