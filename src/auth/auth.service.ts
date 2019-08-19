2feb1b57003289a687df612135f5561863496600
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-19 11:32:00 -0400

    sign in method added to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -11,5 +11,18 @@ export class AuthService {
     // our sign up method
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
        return await this.userRepository.signUp(authCredentialsDto);
+    }
+
+    //our signIn method
+    async signIn(authCredentialsDto: AuthCredentialsDto){
+        //if username exist and password is valid we will get back the username, else undefined
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+
+        //throw an excetion if user does not exist
+        if(!username){
+            throw new UnauthorizedException("Invalid Credentials");
+        }
+
+        //jwt token used here
     }
 }
