591f5494f3e8e7bf0a7a43ccc739aa0a53dbab0e
cottojon <cottojonathan@ymail.com>
2019-08-25 12:36:58 -0400

    subscribe entity/relationship join table created

---
+++ src/subscribe/subscribe.entity.ts
@@ -0,0 +1,32 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Channel } from 'src/channels/channel.entity';
+
+ /*this is our relationship table that we have created to relate users and channels
+ In TypeORM, we can either let typeorm make this table using @JoinTable() or
+ we can creeate this table. I choose to make this table explicity.
+
+ The relationship 'subscribes' is many-to-many  with users and subscribes
+
+*/
+
+@Entity()
+export class Subscribe extends BaseEntity{
+    @PrimaryGeneratedColumn() // p key and auto generated, this should be userId and channelId combined as a superkey
+    id: number;
+
+    @Column()
+    userId: number;
+
+    @Column()
+    channelId: number;
+
+    // this is how we relate the two entities for many-to-many
+    @ManyToOne(type => User, user => user.subscribes)
+    user: User;
+
+
+    @ManyToOne(type => Channel, channel => channel.subscribes)
+    channel: Channel;
+}
+
