78517b53ed164f7c9a9587606bf0d41de84005ea
cottojon <cottojonathan@ymail.com>
2019-08-25 13:14:48 -0400

    import authmodule to use jwt/passport

--- src/subscribe/subscribe.module.ts
+++ src/subscribe/subscribe.module.ts
@@ -3,9 +3,10 @@ import { SubscribeController } from './subscribe.controller';
 import { SubscribeService } from './subscribe.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscribeRepository } from './subscribe.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({ //import the subscribe repository into subscribe module
-  imports: [TypeOrmModule.forFeature([SubscribeRepository])], 
+  imports: [TypeOrmModule.forFeature([SubscribeRepository]), AuthModule], //import auth module to use jwt strategy and paspport
   controllers: [SubscribeController],
   providers: [SubscribeService]
 })
