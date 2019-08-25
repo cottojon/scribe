4d9c4128fc75a02b9181168dcb369496ce3f5592
cottojon <cottojonathan@ymail.com>
2019-08-25 12:54:00 -0400

    subscribe module added into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { ClipsModule } from './clips/clips.module';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule} from '@nestjs/typeorm';
+import { SubscribeModule } from './subscribe/subscribe.module';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ChannelsModule, ClipsModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ChannelsModule, ClipsModule, AuthModule, SubscribeModule],
   controllers: [],
   providers: [],
 })
