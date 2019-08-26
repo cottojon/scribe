81b0ff6060570f0355a662cde08d65d173a31d67
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-26 07:41:55 -0400

    dependency inject like repo into like service

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LikeRepository } from './like.repository';
+import { Like } from './like.entity';
 
 @Injectable()
-export class LikesService {}
+export class LikesService {
+    //dependency inject like repository
+    constructor(@InjectRepository(LikeRepository) private likeRepository: LikeRepository){}
+    
+
+    
+}
