984db6a25ada97643afc0634b21c760cfd89dcfc
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-26 07:55:58 -0400

    import auth module to use jwt/passport in likes routes

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -3,9 +3,10 @@ import { LikesController } from './likes.controller';
 import { LikesService } from './likes.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LikeRepository } from './like.repository';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([LikeRepository])], // import like reposioty for dependency injection in the like.service.ts
+@Module({// import like reposioty for dependency injection in the like.service.ts
+  imports: [TypeOrmModule.forFeature([LikeRepository]), AuthModule],  // import auth module to use jwt/passport for guarding routes
   controllers: [LikesController],
   providers: [LikesService]
 })
