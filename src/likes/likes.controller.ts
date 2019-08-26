f886f347149f32cdbb751638363b39d0c7e4113c
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-26 08:15:48 -0400

    like service and controller updated with

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -10,37 +10,39 @@ import { Like } from './like.entity';
 // this means we will be adding the user info to each httprequest (see jwt.strategy.ts validate method)
 export class LikesController {
     //dependency inject like.service.ts
-    constructor(likesService: LikesService){}
+    constructor(private likesService: LikesService) { }
 
 
-      /*
-     A user  likes a clip_id passed in the url as a parameter
-    */
-   @Post('/:clip_id') //custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
-   userLikesClip(@Param('clip_id', ParseIntPipe) clip_id: number, @GetUser() user: User): Promise<Like> {
-      // return this.subscribeService.subscribeUserToChannel(channel_id, user);
-
-      return;
-   }
-
-
-   /*
-   Grab all likes a user has
-   user info is placed in the http request by jwt.strategy.ts
-   */
-   @Get() //custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
-   getUserLikedClips(@GetUser() user: User): Promise<Like[]> {
-       //return this.subscribeService.getUserSubscribedChannels(user);
-       return;
-   }
-
-   /*
-  unlike a user from the clip_id passed in the url as a parameter
+    /*
+   A user  likes a clip_id passed in the url as a parameter
   */
+    @Post('/:clip_id') //custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
+    userLikesClip(@Param('clip_id', ParseIntPipe) clip_id: number, @GetUser() user: User): Promise<Like> {
+        return this.likesService.userLikesClip(clip_id, user);
 
-   @Delete('/:clip_id')//custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
-   unsubscribeUserFromChannel(@Param('clip_id', ParseIntPipe) clip_id: number, @GetUser() user: User): Promise<void> {
-        // return this.subscribeService.unsubscribeUserToChannel(channel_id, user);
+    }
+
+
+    /*
+    Grab all likes a user has
+    user info is placed in the http request by jwt.strategy.ts
+    */
+    @Get() //custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
+    getUserLikedClips(@GetUser() user: User): Promise<Like[]> {
+        //return this.subscribeService.getUserSubscribedChannels(user);
+        console.log(user);
         return;
-   }
+    }
+
+    /*
+   unlike a user from the clip_id passed in the url as a parameter
+   */
+
+    @Delete('/:clip_id')//custom decorator @GetUser() grabs our user from the httprequest placed by jwt.strategy.ts
+    unsubscribeUserFromChannel(@Param('clip_id', ParseIntPipe) clip_id: number, @GetUser() user: User): Promise<void> {
+        // return this.subscribeService.unsubscribeUserToChannel(channel_id, user);
+        console.log(clip_id);
+        console.log(user);
+        return;
+    }
 }
--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LikeRepository } from './like.repository';
 import { Like } from './like.entity';
+import { User } from '../auth/user.entity';
+import { Clip } from 'src/clips/clip.entity';
 
 @Injectable()
 export class LikesService {
@@ -9,5 +11,26 @@ export class LikesService {
     constructor(@InjectRepository(LikeRepository) private likeRepository: LikeRepository){}
     
 
-    
+    async userLikesClip(clip_id: number, user: User): Promise<Like>{
+        return await this.likeRepository.userLikesClip(clip_id, user);
+    }
+
+
+
+    async getUserLikesByUserId(user: User): Promise<Like[]>{
+        //find all the likes by userId
+        const likes = await this.likeRepository.find({ userId: user.id });
+
+        return likes;
+    }
+
+
+    async userUnlikesClip(clip_id: number, user: User): Promise<void>{
+        //delete the task using the repository
+        const result = await this.likeRepository.delete({ clipId: clip_id, userId: user.id });
+        // if we did not delete anything throw exception
+        if (result.affected === 0) { // affected == the amount of rows deleted
+            throw new NotFoundException(`Clip with ID ${clip_id} not found`);
+        }
+    }
 }
