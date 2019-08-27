f1a8afed27fa9f9522fe680f6b0db5f164125ea4
Jonathan Cotto <cottojonathan@ymail.com>
2019-08-27 12:11:17 -0400

    add import auth module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,10 @@ import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentRepository } from './comment.repository';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([CommentRepository])],
+@Module({ //import comment repository
+  imports: [TypeOrmModule.forFeature([CommentRepository]), AuthModule], //import auth module to use jwt/passport
   controllers: [CommentsController],
   providers: [CommentsService]
 })
